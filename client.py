64bf855dbbbf6c985673b1b1312ad349284104eb
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2016-02-23 18:50:27 -0500

    Beginning to abstract chat client

--- client.py
+++ client.py
@@ -2,20 +2,40 @@ import socket
 import sys
 
 HOST, PORT = "localhost", 9999
+
 data = " ".join(sys.argv[1:])
 
+class RDTPClient():
+    def connect(self, host, port):
+        self.host = host
+        self.port = port
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+
+    def send(self, message):
+        self.socket.sendall(message)
+
+    def recv(self, max_len):
+        return self.socket.recv(max_len)
+
+    def close(self):
+        self.socket.close()
+
 # Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
+    chat_client = RDTPClient()
+    chat_client.connect(HOST, PORT)
+    chat_client.send(data + "\n")
+
     # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
+    chat_client.send(data + "\n")
 
     # Receive data from the server and shut down
-    received = sock.recv(1024)
+    received = chat_client.recv(1024)
 finally:
-    sock.close()
+    chat_client.close()
 
 print "Sent:     {}".format(data)
 print "Received: {}".format(received)
--- rdtp.py
+++ rdtp.py
@@ -1,6 +1,6 @@
 import SocketServer
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+class RDTPHandler(SocketServer.BaseRequestHandler):
     """
     The request handler class for our server.
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
 
     # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = SocketServer.TCPServer((HOST, PORT), RDTPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
