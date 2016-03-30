480d19fbe7597d1adeb00bb3c319330c879f1876
Victor Domene <victordomene@college.harvard.edu>
2016-03-30 08:52:23 -0400

    Adds docs for REST Client

--- rest/rest_client.py
+++ rest/rest_client.py
@@ -4,6 +4,9 @@ import requests
 import sys
 
 def check_session(f):
+    """
+    Wrapper that guarantees that there is a section in place.
+    """
     @wraps(f)
     def wrapper(*args, **kwargs):
         if args[0].session is None:
@@ -14,7 +17,21 @@ def check_session(f):
     return wrapper
 
 class RESTClient(ChatClient):
+    """
+    Implements a subclass of ChatClient, that will use REST as its
+    communication protocol.
+    """
+
     def __init__(self, host, port):
+        """
+        Initializes a ChatClient on the given host and port. Defines
+        some class variables that will be used later on, such as session,
+        username and base_url.
+
+        :param host: The host where this client should connect to
+        :param port: The port that this client should connect to
+        """
+
         ChatClient.__init__(self, host, port)
         self.username = None
         self.session = None
@@ -25,7 +42,14 @@ class RESTClient(ChatClient):
     ###########
 
     def create_account(self, username, password):
-        """Instructs server to create an account with given username and password."""
+        """
+        Instructs server to create an account with given username and password.
+
+        :param username: The username to create
+        :param password: The corresponding password
+
+        :return: 0 on success, or 2 if the username already exists.
+        """
         credentials = {'username': username, 'password': password}
 
         response = requests.post(self.base_url + '/users', json=credentials)
@@ -37,8 +61,15 @@ class RESTClient(ChatClient):
         return 0
 
     def login(self, username, password):
-        """Login with given username and password.
-        Returns boolean."""
+        """
+        Login with given username and password.
+        
+        :param username: The username to create
+        :param password: The corresponding password
+
+        :return: 0 on success, 1 if the login credentials do not match,
+        and 2 for any other error (see __handle_error).
+        """
         # First logout of current account
         if self.session is not None:
             self.logout
@@ -63,10 +94,12 @@ class RESTClient(ChatClient):
 
     @check_session
     def logout(self):
-        """Logout of http-sucks-chat.
-        Returns boolean."""
-        if self.session is None:
-            return 1
+        """
+        Logout of the chat service. Assumes that a session is already in place.
+
+        :return: 0 on success, 1 if there is invalid, 2 for 
+        other possible errors (see __handle_error).
+        """
 
         response = self.session.post(self.base_url + '/logout')
         r = response.json()
@@ -81,6 +114,15 @@ class RESTClient(ChatClient):
 
     @check_session
     def delete_account(self):
+        """
+        Delete an account at this chat service. Assumes that a session
+        is already in place, and tries to delete the account currently
+        logged in.
+
+        :return: 0 on success, 1 if the session is invalid, 2 for
+        other possible errors (see __handle_error).
+        """
+
         response = self.session.delete(self.base_url + '/users/' + self.username)
         r = response.json()
 
@@ -91,6 +133,17 @@ class RESTClient(ChatClient):
 
     @check_session
     def send_user(self, username, message):
+        """
+        Send a message to a user. Assumes that a session is already in place,
+        and tries to send the message with the account currently logged in.
+
+        :param username: The username to which the message will be sent
+        :param message: The message that will be sent
+
+        :return: 0 on success, 1 if the session is invalid, 2 for other 
+        possible errors (see __handle_error).
+        """
+
         msg = {'data': {'message': message}}
         response = self.session.post(self.base_url + '/users/' + username + '/messages', json=msg)
         r = response.json()
@@ -102,7 +155,15 @@ class RESTClient(ChatClient):
 
     @check_session
     def fetch(self):
-        """Fetch new messages from the server."""
+        """
+        Fetch new messages from the server. Assumes that a session is already in place,
+        and tries to fetch messages for the currently logged in account.
+
+        :return: On success, returns a string containing all of the messages (or 
+        "No new messages".) On failure, returns 1 if there was no session in place, and
+        2 for other possible errors (see __handle_error).
+        """
+
         response = self.session.get(self.base_url + '/users/' + self.username + '/messages')
         r = response.json()
 
@@ -132,7 +193,14 @@ class RESTClient(ChatClient):
 
     @check_session
     def create_group(self, group_name):
-        """Instructs server to create an account with some group_id."""
+        """
+        Creates a group with some given name. Assumes that a session is already in place.
+
+        :param group_name: The name of the group to be created
+
+        :return: 0 on success, 1 if the session is invalid, 2 for other possible
+        errors (for instance if the groupname is already taken; see __handle_error).
+        """
         data = {'data': {'group_name': group_name}}
         response = self.session.post(self.base_url + '/groups', json=data)
         r = response.json()
@@ -144,7 +212,18 @@ class RESTClient(ChatClient):
 
     @check_session
     def add_user_to_group(self, username, group_name):
-        """Instructs server to add a user to a group."""
+        """
+        Adds a user to a group. Assumes that a session is already in place;
+        if there is no session, no users can be added to groups (namely, only
+        logged in users can modify groups).
+
+        :param username: The username of the user to be added
+        :param group_name: The name of the group to which we should add
+
+        :return: 0 on success, 1 if the session is invalid, 2 for other possible
+        errors (for instance if the groupname is already taken; see __handle_error).
+        """
+
         data = {'data': {'username': username}}
         response = self.session.post(self.base_url + '/groups/' + group_name + '/users', json=data)
         r = response.json()
@@ -156,6 +235,17 @@ class RESTClient(ChatClient):
 
     @check_session
     def send_group(self, group_name, message):
+        """
+        Send a message to a group. Assumes that a session is already in place,
+        and tries to send the message with the account currently logged in.
+
+        :param group_name: The group to which the message will be sent
+        :param message: The message that will be sent
+
+        :return: 0 on success, 1 if the session is invalid, 2 for other 
+        possible errors (see __handle_error).
+        """
+
         msg = {'data': {'message': message}}
         response = self.session.post(self.base_url + '/groups/' + group_name + '/messages', json=msg)
         r = response.json()
@@ -167,6 +257,18 @@ class RESTClient(ChatClient):
 
     @check_session
     def get_groups(self, wildcard):
+        """
+        Get all groups matching wildcard. Assumes that a session is already in place;
+        only logged in users can perform this operation.
+
+        :param wildcard: The regular expression wildcard that we want to use for 
+        searching groups
+
+        :return: On success, returns a string containing the names of the groups. 
+        On failure, return 1 if the session is invalid and 2 for other 
+        possible errors (see __handle_error).
+        """
+
         wildcard = {'wildcard': wildcard}
         response = self.session.get(self.base_url + '/groups', params=wildcard)
         r = response.json()
@@ -183,6 +285,18 @@ class RESTClient(ChatClient):
 
     @check_session
     def get_users(self, wildcard):
+        """
+        Get all users matching wildcard. Assumes that a session is already in place;
+        only logged in users can perform this operation.
+
+        :param wildcard: The regular expression wildcard that we want to use for 
+        searching users
+
+        :return: On success, returns a string containing the names of the users. 
+        On failure, return 1 if the session is invalid and 2 for other 
+        possible errors (see __handle_error).
+        """
+
         wildcard = {'wildcard': wildcard}
         response = self.session.get(self.base_url + '/users', params=wildcard)
         r = response.json()
@@ -201,6 +315,17 @@ class RESTClient(ChatClient):
     ## HELPER ##
     ############
     def __handle_error(self, r):
+        """
+        Helper function that will parse the response by the HTTP server
+        and return an appropriate error code (within the scope of ChatClient).
+
+        The methods defined in ChatClient will use this error code to print out
+        error messages to users. 
+
+        :param r: The response given by the HTTP server that we are connected to
+        :return: 1 for authentication errors, and 2 for any other error.
+        """
+        
         try:
             status_code = r['errors']['status_code']
         except:
--- rest/rest_errors.py
+++ rest/rest_errors.py
@@ -1,18 +1,51 @@
+"""
+Defines some of the commonly used errors returned by the REST
+protocol.
+"""
+
 import json
 
 def error(status_code, description):
+	""" 
+	Returns a general error message, given a status code and a description.
+
+	:param status_code: The HTTP status code, e.g., 404
+	:param description: The description corresponding to that error, e.g., Not Found
+
+	:return: A JSON containing the status_code and description (following the rules
+			 defined by JSON API [http://jsonapi.org/]) and the status_code
+	"""
     return json.dumps({'errors': {'status_code': status_code, 'description': description}}), status_code
 
 def bad_request():
-    return error(400, 'Bad Request: The request cannot be fulfilled due to bad syntax')
+    """
+    Returns the Bad Request error message. Typically, this happens because the
+    parameters passed in to a HTTP request cannot be parsed (either because
+    the format is incorrect, or because the required parameters are lacking).
+	"""
+	return error(400, 'Bad Request: The request cannot be fulfilled due to bad syntax')
 
 def unauthorized():
+	"""
+	Returns the Unauthorized error message. Typically, this happens because 
+	authentication failed.
+	"""
     return error(401, 'Unauthorized: Authentication credentials missing or incorrect')
 
 def forbidden():
+	"""
+	Returns the Forbidden error message. This should be returned if a user
+	tries to do something they do not have permissions to; e.g., deleting a
+	user account that is not their own.
+	"""
     return error(403, 'Forbidden: You do not have permission to perform this request')
 
 def not_found():
+	"""
+	Returns the Not Found error message. Typically, this is issued when any
+	resource was not found by the server. This could be, for instance, if the
+	user attempts to send a message to a user that does not exist.
+	"""
     return error(404, 'Not Found: The resource you requested could not be found')
 
 def internal_server_error():
